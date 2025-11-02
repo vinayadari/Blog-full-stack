9e9dc0a746dcf7b38518d907a80d9e7d9235d1a8
Adari vinay <vinayadari00@gmail.com>
2025-11-02 11:21:47 +0530

    Delete server directory

--- server/src/routes/aiupdate.js
+++
@@ -1,9 +0,0 @@
-import { enhanceBlog } from "../controllers/aiUpdateContent.js";
-import { Router } from "express";
-import { authMiddleware } from "../middleware/authMiddleware.js";
-
-const enhanceRouter = Router();
-
-enhanceRouter.put("/:id", authMiddleware, enhanceBlog);
-
-export default enhanceRouter;
--- server/src/routes/feed.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import getFeed from "../controllers/feedController.js";
-const feedRoute = Router();
-
-feedRoute.get("/", getFeed);
-
-export default feedRoute;
